6f2fb511b1bbe2374179f531741149ea56863624
idskdltm <idskdltm@naver.com>
2020-04-19 18:08:11 +0900

    하핳

--- recognition/views.py
+++ recognition/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
 def recognition(req):
     return render(req, "recog_Service.html")
+
+def recog(req):
+    cctv = req.POST['cctv']
+    context = {'cctv': cctv}
+    return render(req, "recog_Service.html", context)
