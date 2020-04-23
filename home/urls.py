a438719f4dfafa5161e10692b22376fe16faeab6
민찬 <ahwere@naver.com>
2020-04-23 23:43:55 +0900

    sign up, log in ~ing

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from home import views
 
+app_name = 'home'
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("login/", views.login, name="login"),
--- home/views.py
+++ home/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import json
 from django.views import View
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from home.forms import PostForm
+from django.http import HttpResponse
 # Create your views here.
+
+from django.contrib.auth.forms import UserCreationForm
 
 def index(req) :
     context = {
@@ -14,10 +17,23 @@ def index(req) :
     return render(req, "index.html", context=context)
 
 def login(req):
+    if req.method == 'POST':
+        something
+
+    else:
+        form = 
     return render(req, "login.html")
 
 def register(req):
-    return render(req, "register.html")
+    if req.method == 'POST':
+        form = UserCreationForm(req.POST)
+        if form.is_valid():
+            return HttpResponse(form)
+
+    else:
+        form = UserCreationForm()
+
+    return render(req, "register.html", {'form':form})
 
 def mypage(req):
-    return render(req, "mypage.html")
+    return render(requset, "mypage.html")
