8700ba5a4421453746811c034a5766790577ed19
민찬 <ahwere@naver.com>
2020-05-09 00:10:24 +0900

    모달 맵 추가

--- statistic/views.py
+++ statistic/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 from car.models import Car
 from django.db.models import Count, Q
 # Create your views here.
+
 def statistic(req):
     cur_user = req.user
 
@@ -24,7 +25,7 @@ def stat(req):
         user_name = Profile.objects.get(user=auth.get_user(req))
         cctv = req.POST['cctv']
 
-        dataset = Car.objects.values('model').annotate(something=Count('model'))
+        dataset = Car.objects.values('brand').annotate(something=Count('brand'))
 
         context = {
             'cctv': cctv,
@@ -36,25 +37,3 @@ def stat(req):
     else:
         messages.info(req, '로그인 후 이용가능합니다.')
         return redirect('home:index')
-
-
-# #############################################################
-def test(req):
-    # dataset = Car.objects \
-    #     .values('brand') \
-    #     .annotate(kia_count=Count('brand', filter=Q(brand=True)),
-    #               hyundai_count=Count('brand', filter=Q(brand=False))) \
-    #     .order_by('brand')
-
-    dataset = Car.objects.values('model').annotate(something=Count('model'))
-    # hello = Car.objects.values('model').annotate(nothing=Count('model'))
-
-    return render(req, 'test.html', {'dataset': dataset})
-
-# def ticket_class_view(request):
-#     dataset = Passenger.objects \
-#         .values('ticket_class') \
-#         .annotate(survived_count=Count('ticket_class', filter=Q(survived=True)),
-#                   not_survived_count=Count('ticket_class', filter=Q(survived=False))) \
-#         .order_by('ticket_class')
-#     return render(request, 'ticket_class.html', {'dataset': dataset})
