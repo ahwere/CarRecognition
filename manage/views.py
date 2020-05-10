1f404b39d820c73b5fa923454748dbf466756f1b
idskdltm <idskdltm@naver.com>
2020-05-10 13:09:51 +0900

    관리자 회원관리 ajax 부분 완료

--- manage/views.py
+++ manage/views.py
@@ -31,6 +31,6 @@ def reaAllUser(req):
 
 def reaOneUser(req):
 
-    context = Profile.objects.filter(name=req.GET['id']).values()
+    context = User.objects.filter(id=req.GET['id']).values()
 
     return JsonResponse(list(context), safe=False)
