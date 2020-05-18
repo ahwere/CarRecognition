3faf53ca2ecf3e2dc41ad13ab825dec861ac60a5
yujuhyeon <bgg01580@gmail.com>
2020-05-18 17:49:08 +0900

    1.video upload 2.cctv 관리

--- CarRecognition/settings.py
+++ CarRecognition/settings.py
@@ -136,6 +136,6 @@ MEDIA_URL = '/media/' # 항상 / 로 끝나도록 설정
 # MEDIA_URL = 'http://static.myservice.com/media/' 다른 서버로 media 파일 복사시
 
 # 업로드된 파일을 저장할 디렉토리 경로
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/videos')
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
--- manage/views.py
+++ manage/views.py
@@ -6,7 +6,7 @@ from home.models import Profile
 from django.http import HttpResponseRedirect, JsonResponse
 from django.contrib import messages
 from django.core.files.storage import FileSystemStorage
-
+from recognition.models import Cctv
 # Create your views here.
 def manage(req):
     cur_user = req.user
@@ -15,15 +15,27 @@ def manage(req):
         user = Profile.objects.get(user=auth.get_user(req))
 
         if user.permission == 'admin':
-
+            qs = Cctv.objects.all()
             if req.method == 'POST':
-                uploaded_file = req.FILES['document']
+                uploaded_file = req.FILES.get('video')
+                if uploaded_file==None:
+                    messages.info(req, "잘못된 파일 형식입니다.")
+                    return render(req, "manage.html", {'user': user, 'qs': qs})
+                else:
+                    print(len(uploaded_file.name))
+                    ext=uploaded_file.name[len(uploaded_file.name)-3:len(uploaded_file.name)]
+                    if ext != 'mp4' and ext!='wmv' and ext!='mkv' and ext!='wmv' and ext!='avi' and ext!='MOV' and ext!='FLV':
+                        messages.info(req,"잘못된 파일 형식입니다.")
+                        return render(req, "manage.html", {'user': user, 'qs': qs})
+
+
                 fs = FileSystemStorage()
                 name = fs.save(uploaded_file.name, uploaded_file)
                 url = fs.url(name)
-                return render(req, "manage.html", {'user': user, 'url':url})
+                messages.info(req, "동영상이 업로드 되었습니다.")
+                return render(req, "manage.html", {'user': user, 'url':url, 'qs':qs})
             else:
-                return render(req, "manage.html", {'user': user})
+                return render(req, "manage.html", {'user': user, 'qs':qs})
         else:
             messages.info(req, '관리자가 아닙니다.')
             return redirect('home:index')
@@ -31,9 +43,6 @@ def manage(req):
         messages.info(req, '로그인 후 이용하세요.')
         return redirect("home:index")
 
-
-
-        print(uploaded_file.size)
     return render(req,'manage.html')
 
 def reaAllUser(req):
@@ -74,3 +83,8 @@ def grantUser(req):
     }
     return JsonResponse(context)
 
+def delCctv(req,id):
+    qs = Cctv.objects.get(id=id)
+    qs.delete()
+
+    return redirect('manage:manage')
